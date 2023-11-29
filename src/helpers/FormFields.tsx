7ba6810fad01f79dc574327e536ec4627275359c
dn0418 <daniel.noble0418@gmail.com>
2023-11-29 01:23:43 -0600

    Update page

--- src/helpers/FormFields.tsx
+++ src/helpers/FormFields.tsx
@@ -1,5 +1,4 @@
 import { Controller, useFormContext } from "react-hook-form";
-import { useDynamicForm } from "../context/DynamicForm";
 import { TypeOf, ZodAnyDef, ZodEnumDef, z } from "zod";
 import { Path } from "react-hook-form";
 import {
@@ -14,8 +13,10 @@ import {
   SelectChangeEvent,
   FormHelperText,
 } from "@mui/material";
+import { BsExclamationCircle } from "react-icons/bs";
 import DollarIcon from "../assets/icons/dollar-square.svg?react";
 import { DatePicker, DatePickerProps } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
 
 export type FieldProps<T extends z.ZodObject<any>> = {
   name: Path<TypeOf<T>>;
@@ -26,160 +27,82 @@ export type SelectOption = {
   label: string;
 };
 
-export const SelectInput = <T extends z.ZodObject<any>>({
-  name,
-  fieldDef,
-  defaultValue,
-}: FieldProps<T> & {
-  defaultValue: any;
-  fieldDef: ZodEnumDef;
-} & SelectProps) => {
-  const { metadata = {} } = useDynamicForm();
-  const {
-    control,
-    trigger,
-    formState: { errors },
-  } = useFormContext();
-
-  const initOptions = metadata[name] ? metadata[name]?.options : undefined;
-  const options: SelectOption[] = metadata[name]?.onlyOptions
-    ? Object.entries(metadata[name]?.options as { [k: string]: any }).map(
-        ([k, v]) => ({ label: k, value: v })
-      )
-    : (fieldDef as unknown as ZodEnumDef).values.map((value) => {
-        return {
-          label: initOptions ? initOptions[value] || value : value,
-          value: metadata[name]?.value ?? value,
-        };
-      });
-
-  const isMulti = metadata[name]?.multiple === true;
-
+export const SelectInput = ({
+  options,
+  ...props
+}: SelectProps & { options: any[]; defaultValue: string }) => {
+  const [value, setValue] = useState("");
   return (
-    <Controller
-      name={name}
-      control={control}
-      defaultValue={defaultValue}
-      render={({ field: { onChange, value, ...rest } }) => (
-        <FormControl
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              borderRadius: "10px",
-            },
+    <div className="flex items-center gap-2 w-full">
+      <FormControl
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            borderRadius: "10px",
+          },
+        }}
+      >
+        <InputLabel id="demo-simple-select-label">{props?.label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select-label"
+          value={value}
+          {...props}
+          onChange={(e) => {
+            let value = e.target.value as string;
+            setValue(value);
           }}
-          error={errors[name] ? true : false}
+          // defaultValue={
+          //   value ? (isMulti ? value : value.toString()) : undefined
+          // }
         >
-          <InputLabel id="demo-simple-select-label">
-            {metadata[name]?.label}
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select-label"
-            multiple={isMulti}
-            value={value}
-            label={metadata[name]?.label}
-            onChange={(e: SelectChangeEvent<typeof value>) => {
-              let value: string | string[] = e.target.value;
-              if (typeof value === "string" && isMulti) {
-                value = value.split(",");
-              } else {
-                value = value as string;
-              }
-              onChange(value);
-              trigger(name);
-            }}
-            // defaultValue={
-            //   value ? (isMulti ? value : value.toString()) : undefined
-            // }
-            {...rest}
-          >
-            {options.map((option: any) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </Select>
-          <FormHelperText
-            sx={{
-              color: "#f44336",
-            }}
-          >
-            {errors[name]?.message?.toString()}
-          </FormHelperText>
-        </FormControl>
-      )}
-    />
+          <MenuItem value={props.defaultValue} key={props.defaultValue}>
+            {props.defaultValue}
+          </MenuItem>
+          {options.map((option: any) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+        {/* <FormHelperText
+        sx={{
+          color: "#f44336",
+        }}
+        >
+        {errors[name]?.message?.toString()}
+      </FormHelperText> */}
+      </FormControl>
+      <BsExclamationCircle size={20} color="#79747E" />
+
+    </div>
   );
 };
 
-export type CommonInputFieldTypes =
-  | "text"
-  | "number"
-  | "tel"
-  | "email"
-  | "password"
-  | "quantity"
-  | "money";
-
-export type FieldType =
-  | CommonInputFieldTypes
-  | "date"
-  | "select"
-  | "time"
-  | "file"
-  | "checkbox"
-  | "radio"
-  | "switch";
-
-export const CommonInputField = <T extends z.ZodObject<any>>({
-  defaultValue,
-  fieldType,
-  name,
-}: FieldProps<T> & {
-  defaultValue: any;
-  fieldType: FieldType;
-} & TextFieldProps) => {
-  const { metadata = {} } = useDynamicForm();
-  const {
-    control,
-    trigger,
-    formState: { errors },
-  } = useFormContext();
-
+export const CommonInputField = ({ type, ...props }: TextFieldProps) => {
+  const [value, setValue] = useState();
   return (
-    <Controller
-      name={name}
-      control={control}
-      defaultValue={defaultValue}
-      render={({ field: { onChange, value, ...rest } }) => (
-        <TextField
-          {...rest}
-          {...metadata[name]}
-          value={value}
-          onChange={(e) => {
-            onChange(e.target.value);
-            trigger(name);
-          }}
-          InputProps={{
-            startAdornment:
-              fieldType !== "money" ? (
-                <InputAdornment
-                  // sx={{
-                  // backgroundColor: "#E8DEF8",
-                  // borderRadius: "100px",
-                  // }}
-                  className="bg-background-form rounded-[100px] px-3 py-5"
-                  position="start"
-                >
-                  <DollarIcon />
-                </InputAdornment>
-              ) : undefined,
-            className: "rounded-[20px]",
-          }}
-          error={errors[name] ? true : false}
-          helperText={errors[name]?.message?.toString()}
-        />
-      )}
+    <TextField
+      {...props}
+      value={value}
+      onChange={(e) => {
+        setValue(e.target.value as any);
+      }}
+      InputProps={{
+        startAdornment:
+          type === "money" ? (
+            <InputAdornment
+              // sx={{
+              // backgroundColor: "#E8DEF8",
+              // borderRadius: "100px",
+              // }}
+              className="bg-background-form rounded-[100px] px-3 py-5"
+              position="start"
+            >
+              <DollarIcon />
+            </InputAdornment>
+          ) : undefined,
+        className: "rounded-[20px]",
+      }}
     />
   );
 };
@@ -193,7 +116,6 @@ export const DateInputField = <T extends z.ZodObject<any>>({
   defaultValue: any;
   fieldDef: ZodAnyDef;
 } & DatePickerProps<Date>) => {
-  const { metadata = {} } = useDynamicForm();
   const {
     control,
     trigger,
